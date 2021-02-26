10bca89abdd3facc946ea479b9813e172ad87f24
Lenin Compres <lenincompres@gmail.com>
2021-02-26 13:20:15 -0500

    Update extend.js
    removing P5 stuff

--- extend.js
+++ extend.js
@@ -94,25 +94,3 @@ Array.prototype.minus = function (arr = 0) {
 Array.prototype.times = function (arr = 1) { 
   return this.map((n, i) => n * (typeof arr === 'number' ? arr : arr[i % arr.length]));
 }
-//P5 and RGB
-if (!!window.p5) {
-  var opacate = (c, a) => { // returns color with changed opacity 
-    c = color(c);
-    return color(red(c), green(c), blue(c), 255 * a);
-  };
-  var darken = (c, factor = 1) => { // return a color with changed lightness
-    colorMode(HSL);
-    c = color(c);
-    return color(hue(c), saturation(c), lightness(c) * factor);
-  };
-}
-// toggle [r, g, b] and {r:x, g:y, b:z}
-var rgb = (c, asObj) => { 
-  if (Array.isArray(c)) return c.mapTo(['r', 'g', 'b']);
-  if (!!window.p5) {
-    c = color(c);
-    c = [red(c), green(c), blue(c)];
-  }
-  if (typeof c === 'string') c = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(c).filter((_, i) => i > 0 && 1 < 4).map(v => parseInt(v, 16));
-  return asObj ? rgb(c) : c;
-}
