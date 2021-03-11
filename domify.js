6d21854f2177566817330fde08650f687a505880
Lenin Compres <lenincompres@gmail.com>
2021-03-11 11:48:17 -0500

    Update domify.js
    TIGHT!

--- domify.js
+++ domify.js
@@ -1,5 +1,5 @@
 /*
- ** domify  makes html elements from a JS object (or JSON)
+ ** domify makes html element structures from a JS object model
  ** created by: Lenin Compres
  */
  var domify = (...args) => document.body.domify(...args);
@@ -7,23 +7,20 @@
    this.elt.domify(model, prop, replace, this);
  }
  Element.prototype.domify = function (model, prop, replace = false, p5Elem) {
-   let noneValues = [null, undefined];
-   if (noneValues.includes(model)) return;
-   let reservedProps =['_tag', '_id', 'onready', 'onReady', 'onvalue', '_bind', '_numeric', '_true', '_false', '_binary', '_default'];
-   if (reservedProps.includes(prop)) return;
+   if ([null, undefined].includes(model)) return;
+   if (['_tag', '_id', 'onready', 'onReady', 'onvalue', '_bind', '_numeric', '_true', '_false', '_binary', '_default'].includes(prop)) return;
    if (typeof prop === 'boolean' || !prop) {
      p5Elem = replace;
      replace = prop;
      prop = this;
    }
-   let valueTypes = ['boolean', 'number', 'string'];
-   let isElem = o => !noneValues.includes(o) && !valueTypes.includes(typeof o) && (o.elt || o.tagName);
+   let isElem = o => o && (o.elt || o.tagName);
    if (isElem(replace)) return replace.domify(model, prop, p5Elem);
    if (isElem(prop)) return Object.keys(model).map(key => prop.domify(model[key], key, replace));
    if (replace === true) this.innerHTML = '';
    // attibutes and events
    const IS_ARRAY = Array.isArray(model);
-   const IS_VAL = valueTypes.includes(typeof model);
+   const IS_VAL = ['boolean', 'number', 'string'].includes(typeof model);
    if (typeof prop === 'string' && prop[0] === '_') {
      if (prop === '_html') return (this.innerHTML = model);
      if (prop === '_text') return (this.innerText = model);
@@ -31,13 +28,11 @@
      if (prop === '_style' && !IS_VAL && !IS_ARRAY) return Object.assign(this.style, model);
      return this.setAttribute(prop.slice(1), model);
    }
-   let p5Methods = ['mousePressed', 'doubleClicked', 'mouseWheel', 'mouseReleased', 'mouseClicked', 'mouseMoved', 'mouseOver', 'mouseOut', 'touchStarted', 'touchMoved', 'touchEnded', 'dragOver', 'dragLeave'];
-   if (p5Elem && p5Methods.includes(prop)) return p5Elem[prop](model);
-   let elemEvents = ['onblur', 'onchange', 'oninput', 'onfocus', 'onselect', 'onsubmit', 'onreset', 'onkeydown', 'onkeypress', 'onkeyup', 'onmouseover', 'onmouseout', 'onmousedown', 'onmouseup', 'onmousemove', 'onclick', 'ondblclick', 'onload', 'onerror', 'onunload', 'onresize'];
-   if (elemEvents.includes(prop)) return this[prop] = model;
-   // tag, id and classes from prop
-   let [tag, id, ...cls] = prop.split('_'); // tag_id_classes
-   if (prop.includes('.')) { // "tag#id.classes"
+   if (p5Elem && ['mousePressed', 'doubleClicked', 'mouseWheel', 'mouseReleased', 'mouseClicked', 'mouseMoved', 'mouseOver', 'mouseOut', 'touchStarted', 'touchMoved', 'touchEnded', 'dragOver', 'dragLeave'].includes(prop)) return p5Elem[prop](model);
+   if (['onblur', 'onchange', 'oninput', 'onfocus', 'onselect', 'onsubmit', 'onreset', 'onkeydown', 'onkeypress', 'onkeyup', 'onmouseover', 'onmouseout', 'onmousedown', 'onmouseup', 'onmousemove', 'onclick', 'ondblclick', 'onload', 'onerror', 'onunload', 'onresize'].includes(prop)) return this[prop] = model;
+   // tag, id and classes from prop (tag_id_classes) or (tag#id.classes)
+   let [tag, id, ...cls] = prop.split('_');
+   if (prop.includes('.')) {
      cls = prop.split('.');
      tag = cls.shift();
    }
@@ -46,8 +41,7 @@
    // new element
    let elem = isElem(model) ? model : false;
    if (!elem) {
-     let elemTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'main', 'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'datalist', 'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'map', 'mark', 'meta', 'meter', 'menu', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'progress', 'q', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'u', 'ul', 'var', 'video', 'wbr'];
-     if (!elemTags.includes(tag)) {
+     if (!['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'main', 'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'datalist', 'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'map', 'mark', 'meta', 'meter', 'menu', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'progress', 'q', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'u', 'ul', 'var', 'video', 'wbr'].includes(tag)) {
        id = tag;
        tag = 'div';
      }
@@ -58,7 +52,7 @@
        else Object.keys(model).map(key => elem.domify(model[key], key)); 
      }
    }
-   // handles id and appends element
+   // id and appending element
    if (id = model._id ? model._id : id) {
      if (!IS_ARRAY) elem && p5Elem ? elem.id(id) : elem.setAttribute('id', id);
      window[id] = elem;
