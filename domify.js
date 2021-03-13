46ae1964ed7cc9d1a08e2d95ca1da828c80b5a2f
Lenin Compres <lenincompres@gmail.com>
2021-03-13 14:15:34 -0500

    Update domify.js
    makes sure it only clears once when called with multiple elems.

--- domify.js
+++ domify.js
@@ -13,7 +13,11 @@ Element.prototype.domify = function (model, a1, a2, p5Elem) {
   let append = typeof a1 === 'boolean' ? a1 : typeof a2 === 'boolean' ? a2 : typeof p5Elem === 'boolean' ? p5Elem : undefined;
   let elt = a1 && a1.tagName ? a1 : a2 && a2.tagName ? a2 : false;
   if (elt) return elt.domify(model, prop, append);
-  if (!prop) return Object.keys(model).map(key => this.domify(model[key], key, append));
+  if (!prop) {
+    let elems = Object.keys(model).map((key, i) => this.domify(model[key], key, true)).filter(e => e.tagName);
+    if(elems.length && append === undefined) this.innerHTML = '';
+    return elems.forEach(e => this[append===false ? 'prepend' : 'append'](e));
+  }
   // attibutes and events
   let tagName = this.tagName.toLowerCase();
   const IS_HEAD = tagName === 'head';
@@ -26,17 +30,12 @@ Element.prototype.domify = function (model, a1, a2, p5Elem) {
     if (!IS_HEAD && this.style[prop] !== undefined) return this.style[prop] = model;
     if (!(IS_HEAD && ['title', 'style'].includes(prop)) && ((prop === 'span' && tagName.startsWith('col')) || ['accept', 'accept-charset', 'accesskey', 'action', 'align', 'alt', 'async', 'autocomplete', 'autofocus', 'autoplay', 'bgcolor', 'border', 'charset', 'checked', 'cite', 'class', 'color', 'cols', 'colspan', 'content', 'contenteditable', 'controls', 'coords', 'data', 'datetime', 'default', 'defer', 'dir', 'dirname', 'disabled', 'download', 'draggable', 'enctype', 'for', 'form', 'formaction', 'headers', 'height', 'hidden', 'high', 'href', 'hreflang', 'http-equiv', 'id', 'ismap', 'kind', 'label', 'lang', 'list', 'loop', 'low', 'max', 'maxlength', 'media', 'method', 'min', 'multiple', 'muted', 'name', 'novalidate', 'open', 'optimum', 'pattern', 'placeholder', 'poster', 'preload', 'readonly', 'rel', 'required', 'reversed', 'rows', 'rowspan', 'sandbox', 'scope', 'selected', 'shape', 'size', 'sizes', 'spellcheck', 'src', 'srcdoc', 'srclang', 'srcset', 'start', 'step', 'style', 'tabindex', 'target', 'title', 'translate', 'type', 'usemap', 'value', 'width', 'wrap'].includes(prop) || prop.startsWith('data-'))) return this.setAttribute(prop, model);
   } else {
-    if (IS_HEAD && prop === 'style') return this.domify(`${Object.keys(model).map(key => `${key} {${Object.keys(model[key]).map(k => `${k.camelCase('-')}:${model[key][k]}`).join(';')}`).join('}')}}`, 'style', append);
+    if (IS_HEAD && prop === 'style') return this.domify(`${Object.keys(model).map(key => `${key} {${Object.keys(model[key]).map(k => `${k.camelCase('-')}:${model[key][k]}`).join(';')}`).join('}')}}`, 'style', true);
     if (IS_ARRAY && prop === 'class') return this.classList.add(...model);
     if (!IS_HEAD && !IS_ARRAY && prop === 'style') return Object.assign(this.style, model);
     if (IS_FUNCTION && this[prop] !== undefined) return this[prop] = model;
     if (p5Elem && IS_FUNCTION && p5Elem[prop] !== undefined) return p5Elem[prop](model);
   }
-  if (append === undefined) {
-    this.innerHTML = '';
-    append = true;
-  }
-  let addMethod = append ? 'append' : 'prepend';
   // tag, id and classes from prop (tag_id_classes) or (tag#id.classes)
   let [tag, id, ...cls] = prop.split('_');
   if (prop.includes('.')) {
@@ -46,6 +45,11 @@ Element.prototype.domify = function (model, a1, a2, p5Elem) {
   if (tag.includes('#'))[tag, id] = tag.split('#');
   tag = model.tag ? model.tag : tag;
   // new element
+  if (append === undefined) {
+    this.innerHTML = '';
+    append = true;
+  }
+  let addMethod = append ? 'append' : 'prepend';
   let elem = (model.tagName || model.elt) ? model : false;
   if (!elem) {
     if (!['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'main', 'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'datalist', 'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'map', 'mark', 'meta', 'meter', 'menu', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'progress', 'q', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'u', 'ul', 'var', 'video', 'wbr'].includes(tag)) {
