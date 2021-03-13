9fd66cfd088f797de442874cb80dd03b786fea53
Lenin Compres <lenincompres@gmail.com>
2021-03-13 13:36:17 -0500

    Update domify.js
    Clears the element by default when any subtags are in the model.
    Appends when passed true.
    Prepends when passed false.

--- domify.js
+++ domify.js
@@ -3,15 +3,17 @@
  ** created by: Lenin Compres
  */
 var domify = (...args) => document.body.domify(...args);
-if (typeof p5 !== 'undefined') p5.element.domify = (model, prop, replace) => this.elt.domify(model, prop, replace, this);
+Element.prototype.modify = function (...args) {
+  this.domify(...args)
+}
 Element.prototype.domify = function (model, a1, a2, p5Elem) {
   if ([null, undefined].includes(model)) return;
   let prop = typeof a1 === 'string' ? a1 : typeof a2 === 'string' ? a2 : false;
-  if (['tag', 'id', 'onready', 'onReady', 'onvalue', 'bind', 'numeric', 'binary', 'value'].includes(prop)) return;
-  let clear = typeof a1 === 'boolean' ? a1 : typeof a2 === 'boolean' ? a2 : false;
+  if (['tag', 'id', 'onready', 'onReady'].includes(prop)) return;
+  let append = typeof a1 === 'boolean' ? a1 : typeof a2 === 'boolean' ? a2 : typeof p5Elem === 'boolean' ? p5Elem : undefined;
   let elt = a1 && a1.tagName ? a1 : a2 && a2.tagName ? a2 : false;
-  if (elt) return elt.domify(model, prop, clear);
-  if (!prop) return Object.keys(model).map(key => this.domify(model[key], key, clear));
+  if (elt) return elt.domify(model, prop, append);
+  if (!prop) return Object.keys(model).map(key => this.domify(model[key], key, append));
   // attibutes and events
   let tagName = this.tagName.toLowerCase();
   const IS_HEAD = tagName === 'head';
@@ -24,12 +26,17 @@ Element.prototype.domify = function (model, a1, a2, p5Elem) {
     if (!IS_HEAD && this.style[prop] !== undefined) return this.style[prop] = model;
     if (!(IS_HEAD && ['title', 'style'].includes(prop)) && ((prop === 'span' && tagName.startsWith('col')) || ['accept', 'accept-charset', 'accesskey', 'action', 'align', 'alt', 'async', 'autocomplete', 'autofocus', 'autoplay', 'bgcolor', 'border', 'charset', 'checked', 'cite', 'class', 'color', 'cols', 'colspan', 'content', 'contenteditable', 'controls', 'coords', 'data', 'datetime', 'default', 'defer', 'dir', 'dirname', 'disabled', 'download', 'draggable', 'enctype', 'for', 'form', 'formaction', 'headers', 'height', 'hidden', 'high', 'href', 'hreflang', 'http-equiv', 'id', 'ismap', 'kind', 'label', 'lang', 'list', 'loop', 'low', 'max', 'maxlength', 'media', 'method', 'min', 'multiple', 'muted', 'name', 'novalidate', 'open', 'optimum', 'pattern', 'placeholder', 'poster', 'preload', 'readonly', 'rel', 'required', 'reversed', 'rows', 'rowspan', 'sandbox', 'scope', 'selected', 'shape', 'size', 'sizes', 'spellcheck', 'src', 'srcdoc', 'srclang', 'srcset', 'start', 'step', 'style', 'tabindex', 'target', 'title', 'translate', 'type', 'usemap', 'value', 'width', 'wrap'].includes(prop) || prop.startsWith('data-'))) return this.setAttribute(prop, model);
   } else {
+    if (IS_HEAD && prop === 'style') return this.domify(`${Object.keys(model).map(key => `${key} {${Object.keys(model[key]).map(k => `${k.camelCase('-')}:${model[key][k]}`).join(';')}`).join('}')}}`, 'style', append);
     if (IS_ARRAY && prop === 'class') return this.classList.add(...model);
     if (!IS_HEAD && !IS_ARRAY && prop === 'style') return Object.assign(this.style, model);
     if (IS_FUNCTION && this[prop] !== undefined) return this[prop] = model;
     if (p5Elem && IS_FUNCTION && p5Elem[prop] !== undefined) return p5Elem[prop](model);
   }
-  if (clear === true) this.innerHTML = '';
+  if (append === undefined) {
+    this.innerHTML = '';
+    append = true;
+  }
+  let addMethod = append ? 'append' : 'prepend';
   // tag, id and classes from prop (tag_id_classes) or (tag#id.classes)
   let [tag, id, ...cls] = prop.split('_');
   if (prop.includes('.')) {
@@ -45,11 +52,12 @@ Element.prototype.domify = function (model, a1, a2, p5Elem) {
       id = tag;
       tag = 'div';
     }
-    if (IS_ARRAY) elem = model.map(o => this.domify(o, [tag, ...cls].join('.')));
+    if (IS_ARRAY) elem = model.map(o => this.domify(o, [tag, ...cls].join('.'), append));
     else {
-      elem = p5Elem ? createElement(tag) : this.appendChild(document.createElement(tag));
+      elem = p5Elem ? createElement(tag) : document.createElement(tag);
+      this[addMethod](elem);
       if (IS_VALUE)(p5Elem ? elem.elt : elem).innerHTML = model; // no children
-      else Object.keys(model).map(key => elem.domify(model[key], key));
+      else Object.keys(model).map(key => elem.domify(model[key], key, append));
     }
   }
   // id and appending element
@@ -61,7 +69,10 @@ Element.prototype.domify = function (model, a1, a2, p5Elem) {
   if (cls) cls.forEach(c => p5Elem ? elem.addClass(c) : elem.classList.add(c));
   let onready = model.onready ? model.onready : model.onReady;
   if (onready) onready(elem);
-  p5Elem ? p5Elem.child(elem) : this.appendChild(elem);
-  if (typeof domifyBind === 'function' && id) window[id] = domifyBind(p5Elem ? elem.elt : elem, model);
+  p5Elem ? p5Elem.child(elem) : this[addMethod](elem);
   return elem;
 };
+String.prototype.camelCase = function (s = false) {
+  return s ? this.replace(/([A-Z])/g, s + '$1').toLowerCase() : this.replace(/^([A-Z])|[\s-_ ]+(\w)/g, (match, p1, p2) => p2 ? p2.toUpperCase() : p1.toLowerCase());
+}
+if (typeof p5 !== 'undefined') p5.element.domify = (model, a1, a2) => this.elt.domify(model, a1, a2, this);
