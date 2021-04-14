134a678f59185f46b9576ff98c93cac6b8041883
Lenin Compres <lenincompres@gmail.com>
2021-04-13 22:13:51 -0400

    Update domify.js

--- domify.js
+++ domify.js
@@ -8,9 +8,10 @@ const domify = (...args) => document.body ? document.body.domify(...args) : wind
 Element.prototype.domify = Element.prototype.modify = function (structure, ...args) {
   if ([null, undefined].includes(structure)) return;
   let station = args.filter(a => typeof a === 'string')[0]; // style|attr|tag|inner…|onEvent|name
-  if (['tag', 'id', 'onready', 'onReady'].includes(station)) return;
+  if (['tag', 'onready'].includes(station)) return;
+  if (['text', 'innerText'].includes(station)) return this.innerText = structure;
   if (typeof structure == 'string' && structure.endsWith('.json')) return domloadRequest(structure, data => this.domify(data, ...args), error => console.log(structure, 'Could not load JSON file.'));
-  const IS_PRIMITIVE = isPrimitive(structure);
+  const IS_PRIMITIVE = ['boolean', 'number', 'string'].includes(typeof structure);
   const IS_FUNCTION = typeof structure === 'function';
   const IS_ARRAY = Array.isArray(structure);
   const TAG = this.tagName.toLowerCase();
@@ -20,8 +21,10 @@ Element.prototype.domify = Element.prototype.modify = function (structure, ...ar
   if (elt) return elt.domify(structure, station, CLEAR, p5Elem);
   let p5Elem = args.filter(a => a && a.elt)[0];
   const PREPEND = CLEAR === false;
-  if (!station) {
-    if (CLEAR) this.innerHTML = '';
+  if (!structure.bind && (station === 'content' &&  TAG !== 'meta') || station === 'innerHTML') station = 'html';
+  if (!station || station === 'html') {
+    if (IS_PRIMITIVE) return this.innerHTML = structure;
+    if (CLEAR || station === 'html') this.innerHTML = '';
     let keys = PREPEND ? Object.keys(structure).reverse() : Object.keys(structure);
     keys.forEach(key => this.domify(structure[key], key, p5Elem, PREPEND ? false : undefined));
     return this;
@@ -34,22 +37,21 @@ Element.prototype.domify = Element.prototype.modify = function (structure, ...ar
   }
   if (tag.includes('#'))[tag, id] = tag.split('#');
   tag = (structure.tag ? structure.tag : tag).toLowerCase();
+  const IS_TAG = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'main', 'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'datalist', 'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'frame', 'frameset', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'map', 'mark', 'meta', 'meter', 'menu', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'progress', 'q', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'u', 'ul', 'var', 'video', 'wbr'].includes(tag);
+  const IS_ATTRIBUTE = ['accept', 'accept-charset', 'accesskey', 'action', 'align', 'alt', 'async', 'autocomplete', 'autofocus', 'autoplay', 'bgcolor', 'border', 'charset', 'checked', 'cite', 'class', 'color', 'cols', 'colspan', 'content', 'contenteditable', 'controls', 'coords', 'data', 'datetime', 'default', 'defer', 'dir', 'dirname', 'disabled', 'download', 'draggable', 'enctype', 'for', 'form', 'formaction', 'headers', 'height', 'hidden', 'high', 'href', 'hreflang', 'http-equiv', 'id', 'ismap', 'kind', 'lang', 'list', 'loop', 'low', 'max', 'maxlength', 'media', 'method', 'min', 'multiple', 'muted', 'name', 'novalidate', 'open', 'optimum', 'pattern', 'placeholder', 'poster', 'preload', 'readonly', 'rel', 'required', 'reversed', 'rows', 'rowspan', 'sandbox', 'scope', 'selected', 'shape', 'size', 'sizes', 'spellcheck', 'src', 'srcdoc', 'srclang', 'srcset', 'start', 'step', 'style', 'tabindex', 'target', 'title', 'translate', 'type', 'usemap', 'value', 'wrap', 'width'].includes(station);
   if (IS_ARRAY) {
     if (station === 'class') return structure.forEach(c => c ? this.classList.add(c) : null);
     if (station === 'addEventListener') return this.addEventListener(...structure);
     let map = structure.map(s => this.domify(s, tag + '.' + cls.join('.')));
     if (id) window[id] = map;
-    return
+    return;
   }
+  if (station === 'addEventListener') return structure.options ? this.addEventListener(structure.type, structure.listener, structure.options) : this.addEventListener(structure.type, structure.listener, structure.useCapture, structure.wantsUntrusted);
   if (structure.bind && !IS_FUNCTION) {
-    if (!HTML_TAGS.includes(tag)) {
-      structure.bind.bind(this, station, structure.onvalue);
-      if (structure.value !== undefined) structure.bind.value = structure.value;
-      return;
-    }
-    structure = {
-      content: structure
-    }
+    if (!IS_ATTRIBUTE && this.style[station] === undefined) return this.domify(structure, 'content');
+    structure.bind.bind(this, station, structure.onvalue);
+    if (structure.value !== undefined) structure.bind.value = structure.value;
+    return;
   }
   if (tag === 'style') {
     if (IS_PRIMITIVE && !IS_HEAD) return this.setAttribute(tag, structure);
@@ -62,15 +64,13 @@ Element.prototype.domify = Element.prototype.modify = function (structure, ...ar
       if (CLEAR) this.style = '';
       return Object.keys(structure).forEach(k => this.style[k] = structure[k]);
     }
-    if (!isPrimitive(structure.content)) structure.content = domifyCSS(structure.content);
+    if (!['boolean', 'number', 'string'].includes(typeof structure.content)) structure.content = domifyCSS(structure.content);
   }
   if (IS_FUNCTION) {
     if (p5Elem && typeof p5Elem[station] === 'function') return p5Elem[station](structure);
     return this[station] = structure;
   }
   if (IS_PRIMITIVE) {
-    if (TAG !== 'meta' && ['content', 'html', 'innerHTML'].includes(station)) return this.innerHTML = structure;
-    if (['text', 'innerText'].includes(station)) return this.innerText = structure;
     if (IS_HEAD) {
       let extension = typeof structure === 'string' ? structure.split('.').slice(-1)[0] : 'none';
       if (station === 'title') return this.innerHTML += `<${station}>${structure}</${station}>`;
@@ -87,82 +87,26 @@ Element.prototype.domify = Element.prototype.modify = function (structure, ...ar
       if (station === 'script' && extension === 'js') return this.innerHTML += `<script src="${structure}"</script>`;
     }
     let done = this.style[station] !== undefined ? this.style[station] = structure : undefined;
-    if (HTML_ATTRIBUTES.includes(station)) {
-      this.setAttribute(station, structure);
-      done = true;
-    }
+    done = IS_ATTRIBUTE ? !this.setAttribute(station, structure) : done;
+    if (station === 'id') window[station] = this;
     if (done !== undefined) return;
   }
-  if (['content', 'html', 'innerHTML'].includes(station)) return this.domify(structure, true);
   let elem = (structure.tagName || structure.elt) ? structure : false;
   if (!elem) {
-    if (!HTML_TAGS.includes(tag)) {
-      id = tag;
-      tag = 'div';
-    }
+    if (!tag) tag = 'div';
     elem = p5Elem ? createElement(tag) : document.createElement(tag);
-    if (IS_PRIMITIVE)(p5Elem ? elem.elt : elem).innerHTML = structure;
-    else Object.keys(structure).map(key => elem.domify(structure[key], key, p5Elem));
+    elem.domify(structure, p5Elem);
   }
-  elt = (p5Elem ? elem.elt : elem);
-  if (cls) cls.forEach(c => c ? elt.classList.add(c) : null);
-  if (id = structure.id ? structure.id : id) {
-    elt.setAttribute('id', id);
-    window[id] = elem;
+  elt = p5Elem ? elem.elt : elem;
+  if (cls) {
+    elt.domify(id, 'id');
+    cls.forEach(c => c ? elt.classList.add(c) : null);
   }
   this[PREPEND ? 'prepend' : 'append'](elt);
   if (structure.onready) structure.onready(elem);
   return elem;
 };
 
-/* ---------- AUXILIARY MEHODS ------------- */
-
-const HTML_ATTRIBUTES = ['accept', 'accept-charset', 'accesskey', 'action', 'align', 'alt', 'async', 'autocomplete', 'autofocus', 'autoplay', 'bgcolor', 'border', 'charset', 'checked', 'cite', 'class', 'color', 'cols', 'colspan', 'content', 'contenteditable', 'controls', 'coords', 'data', 'datetime', 'default', 'defer', 'dir', 'dirname', 'disabled', 'download', 'draggable', 'enctype', 'for', 'form', 'formaction', 'headers', 'height', 'hidden', 'high', 'href', 'hreflang', 'http-equiv', 'id', 'ismap', 'kind', 'lang', 'list', 'loop', 'low', 'max', 'maxlength', 'media', 'method', 'min', 'multiple', 'muted', 'name', 'novalidate', 'open', 'optimum', 'pattern', 'placeholder', 'poster', 'preload', 'readonly', 'rel', 'required', 'reversed', 'rows', 'rowspan', 'sandbox', 'scope', 'selected', 'shape', 'size', 'sizes', 'spellcheck', 'src', 'srcdoc', 'srclang', 'srcset', 'start', 'step', 'style', 'tabindex', 'target', 'title', 'translate', 'type', 'usemap', 'value', 'wrap', 'width'];
-const HTML_TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'main', 'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'datalist', 'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'frame', 'frameset', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'map', 'mark', 'meta', 'meter', 'menu', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'progress', 'q', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'u', 'ul', 'var', 'video', 'wbr'];
-const PSEUDO_CLASSES = ['active', 'checked', 'disabled', 'empty', 'enabled', 'first-child', 'first-of-type', 'focus', 'hover', 'in-range', 'invalid', 'last-of-type', 'link', 'only-of-type', 'only-child', 'optional', 'out-of-range', 'read-only', 'read-write', 'required', 'root', 'target', 'valid', 'visited', 'lang', 'not', 'nth-child', 'nth-last-child', 'nth-last-of-type', 'nth-of-type'];
-const PSEUDO_ELEMENTS = ['after', 'before', 'first-letter', 'first-line', 'selection'];
-
-String.prototype.camelCase = function (s = false) {
-  return s ? this.replace(/([A-Z])/g, s + '$1').toLowerCase() : this.replace(/^([A-Z])|[\s-_ ]+(\w)/g, (_, p1, p2) => p2 ? p2.toUpperCase() : p1.toLowerCase());
-};
-
-const isPrimitive = foo => ['boolean', 'number', 'string'].includes(typeof foo);
-
-// consolidates structurals objects in an array into one object with all properties
-const assignAll = (source = {}, dest = {}) => {
-  (Array.isArray(source) ? source : [source]).forEach(s => Object.assign(dest, s));
-  return dest;
-}
-
-// turns a structural object into css
-const domifyCSS = (sel, obj) => {
-  if (typeof sel !== 'string') {
-    if (!sel) return;
-    if (Array.isArray(sel)) sel = assignAll(sel);
-    return Object.keys(sel).map(key => domifyCSS(key, sel[key])).join(' ');
-  }
-  let extra = [],
-    id, cls;
-  [sel, ...cls] = sel.split('_');
-  if (sel.toLowerCase() === 'fontface') sel = '@font-face';
-  //if (id) sel += '#' + id;
-  if (cls.length) sel += '.' + cls.join('.');
-  if (isPrimitive(obj)) return `${sel.camelCase('-')}: ${obj};`;
-  if (Array.isArray(obj)) obj = assignAll(obj);
-  let css = Object.keys(obj).map(key => {
-    let style = obj[key];
-    if (isPrimitive(style)) return domifyCSS(key, style);
-    let sub = key.split('(')[0].camelCase('-');
-    let xSel = `${sel} ${key}`;
-    if (PSEUDO_CLASSES.includes(sub)) xSel = `${sel}:${key}`;
-    else if (PSEUDO_ELEMENTS.includes(sub)) xSel = `${sel}::${key}`;
-    else if (style.immediate) xSel = `${sel}>${key}`;
-    extra.push(domifyCSS(xSel, style));
-  }).join(' ');
-  return (css ? `\n${sel} {${css}} ` : '') + extra.join(' ');
-}
-
-// makes domify available for p5
 if (typeof p5 !== 'undefined') {
   p5.domify = (...args) => domify(...args, createDiv());
   p5.Element.prototype.domify = p5.Element.prototype.modify = function (...args) {
@@ -170,22 +114,38 @@ if (typeof p5 !== 'undefined') {
   }
 }
 
-// used to bind element properties, attributes, styles or content
-
-const dombind = (name, onvalue, value) => {
-  let bind = window[name] ? window[name] : window[name] = new Bind();
-  const IS_FUNC = typeof onvalue === 'function'
-  return {
-    bind: bind,
-    onvalue: IS_FUNC ? onvalue : value,
-    value: IS_FUNC ? value : onvalue
+const domifyCSS = (sel, obj) => {
+  // consolidates structurals objects in an array into one object with all properties
+  const assignAll = (arr = [], dest = {}) => {
+    arr.forEach(prop => Object.assign(dest, prop));
+    return dest;
   }
-}
-
-const domload = (url, onload, value) => {
-  let obj = dombind(url, onload, value);
-  domloadRequest(url, data => !data ? null : obj.bind.value = data);
-  return obj;
+  if (typeof sel !== 'string') {
+    if (!sel) return;
+    if (Array.isArray(sel)) sel = assignAll(sel);
+    return Object.keys(sel).map(key => domifyCSS(key, sel[key])).join(' ');
+  }
+  const unCamel = (str) => str.replace(/([A-Z])/g, '-' + '$1').toLowerCase();
+  let extra = [];
+  let cls = sel.split('_');
+  sel = cls.shift();
+  if (sel.toLowerCase() === 'fontface') sel = '@font-face';
+  if (cls.length) sel += '.' + cls.join('.');
+  if (['boolean', 'number', 'string'].includes(typeof obj))
+    return `${unCamel(sel)}: ${obj};`;
+  if (Array.isArray(obj)) obj = assignAll(obj);
+  let css = Object.keys(obj).map(key => {
+    let style = obj[key];
+    if (['boolean', 'number', 'string'].includes(typeof style))
+      return domifyCSS(key, style);
+    let sub = unCamel(key.split('(')[0]);
+    let xSel = `${sel} ${key}`;
+    if (['active', 'checked', 'disabled', 'empty', 'enabled', 'first-child', 'first-of-type', 'focus', 'hover', 'in-range', 'invalid', 'last-of-type', 'link', 'only-of-type', 'only-child', 'optional', 'out-of-range', 'read-only', 'read-write', 'required', 'root', 'target', 'valid', 'visited', 'lang', 'not', 'nth-child', 'nth-last-child', 'nth-last-of-type', 'nth-of-type'].includes(sub)) xSel = `${sel}:${key}`;
+    else if (['after', 'before', 'first-letter', 'first-line', 'selection'].includes(sub)) xSel = `${sel}::${key}`;
+    else if (style.immediate) xSel = `${sel}>${key}`;
+    extra.push(domifyCSS(xSel, style));
+  }).join(' ');
+  return (css ? `\n${sel} {${css}} ` : '') + extra.join(' ');
 }
 
 class Bind {
@@ -210,8 +170,16 @@ class Bind {
     return this._value;
   }
 }
+const dombind = (name, onvalue, value) => {
+  let bind = window[name] !== undefined ? window[name] : window[name] = new Bind();
+  const IS_FUNC = typeof onvalue === 'function'
+  return {
+    bind: bind,
+    onvalue: IS_FUNC ? onvalue : value,
+    value: IS_FUNC ? value : onvalue
+  }
+}
 
-// XMLHttpRequest JSON
 const domloadRequest = (url, data, onsuccess = _ => null, onerror = _ => null) => {
   const GET = data === false;
   if (typeof data === 'function') {
@@ -220,23 +188,19 @@ const domloadRequest = (url, data, onsuccess = _ => null, onerror = _ => null) =
     data = {};
   }
   let xobj = new XMLHttpRequest();
-  xobj.onreadystatechange = _ => {
-    if (xobj.readyState == 4 && xobj.status == '200') {
-      try {
-        onsuccess(JSON.parse(xobj.responseText));
-      } catch (e) {
-        onsuccess(xobj.responseText); //none parsed String
-      }
-    } else onerror(xobj.status);
-  }
+  xobj.onreadystatechange = _ => xobj.readyState == 4 && xobj.status == '200' ?
+    onsuccess(xobj.responseText) : onerror(xobj.status);
   xobj.open(GET ? 'POST' : 'GET', url, true);
   xobj.send(data);
 };
+const domload = (url, onload, value) => {
+  let obj = dombind(url, onload, value);
+  domloadRequest(url, data => data !== undefined ? obj.bind.value = data : null);
+  return obj;
+}
 
 // initializes the dom and head automatically if there's an ini.json and/or a main.js
-domloadRequest('ini.json', data => dominify(data, true), error => error === 404 ? dominify(false, true) : null);
-
-let dominify = (INI, autoCall = false) => {
+let dominify = (INI) => {
   const ENTRY_POINT = 'main.js';
   const makeArray = a => Array.isArray(a) ? a : [a];
   let ini = Object.assign({
@@ -254,15 +218,7 @@ let dominify = (INI, autoCall = false) => {
     entryPoint: ENTRY_POINT,
     module: true,
     postscript: []
-  }, isPrimitive(INI) ? {} : INI);
-  const N = 6;
-  const EM_MAX = 2;
-  let hn = {};
-  (new Array(N)).fill('h').forEach((h,i) => {
-    hn[h + (i+1)] = {
-      fontSize: (EM_MAX-i/N) + 'em'
-    }
-  });
+  }, ['boolean', 'number', 'string'].includes(typeof INI) ? {} : INI);
   if (INI) {
     let reset = {
       '*': {
@@ -294,8 +250,17 @@ let dominify = (INI, autoCall = false) => {
         fontStyle: 'itallic',
       }
     };
-    Object.assign(reset, hn)
-    if (INI) document.head.domify({
+    const N = 6,
+      EM_MAX = 2;
+    let hn = {};
+    (new Array(N)).fill('h').forEach((h, i) => {
+      hn[h + (i + 1)] = {
+        fontSize: (EM_MAX - i / N) + 'em',
+        lineHeight: '1em',
+      }
+    });
+    Object.assign(reset, hn);
+    document.head.domify({
       title: ini.title,
       meta: [{
         charset: ini.charset
@@ -311,13 +276,13 @@ let dominify = (INI, autoCall = false) => {
       style: [ini.resetCSS ? reset : null, ...makeArray(ini.style)]
     }, true);
   }
-  // sets the entry point Should only be done once
-  if (autoCall) domloadRequest(INI ? ini.entryPoint : ENTRY_POINT, data => {
+  domloadRequest(ini.entryPoint, _ => {
     domify({
       script: [{
         type: ini.module ? 'module' : null,
-        src: ini.entry ? ini.entry : ini.entryPoint
+        src: ini.entryPoint
       }, ...makeArray(ini.postscript)]
     });
   });
 };
+domloadRequest('ini.json', data => dominify(JSON.parse(data)), error => error === 404 ? dominify() : null);
